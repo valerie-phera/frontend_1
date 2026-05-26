3157d0b063c3e6ba375a28dcaae754447a7653a8
Ruslan_Sokolov <vaquero1master@gmail.com>
2026-05-26 13:14:40 +0200

    fix icons

--- src/assets/icons/CheckIcon.jsx
+++ src/assets/icons/CheckIcon.jsx
@@ -1,31 +1,15 @@
 const CheckIcon = (props) => (
-    <svg
-        width="10"
-        height="10"
-        viewBox="0 0 10 10"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        {...props}
-    >
-        {/* контур круга */}
-        <circle
-            cx="5"
-            cy="5"
-            r="4.5"
-            stroke="black"
-            strokeWidth="0.5"
-            fill="none"
-        />
-
-        {/* галочка */}
-        <path
-            d="M3 5.2 L4.3 6.5 L7 3.8"
-            stroke="black"
-            strokeWidth="0.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        />
-    </svg>
-);
-
-export default CheckIcon;
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={12}
+    height={12}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#00343D"
+      d="m10.898 3.773-6 6a.563.563 0 0 1-.797 0L1.476 7.148a.563.563 0 0 1 .797-.797L4.5 8.578l5.602-5.6a.564.564 0 0 1 .797.796l-.001-.001Z"
+    />
+  </svg>
+)
+export default CheckIcon
